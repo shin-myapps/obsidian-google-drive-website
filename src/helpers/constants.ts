888fb5d397cf2f08109cd6a3fce1de59b03c0397
shin-myapps <shin.myapps@gmail.com>
2025-07-09 12:52:12 +0800

    Update constants.ts
    update with my own google's Client ID

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,6 @@
 export const scopes = ['https://www.googleapis.com/auth/drive.file'];
 
 export const clientId =
-  '873289301374-t92pvi7grsn2bubatj623m8pof202fdh.apps.googleusercontent.com';
+  '652914244381-hlu6pogp9kohsbkv0aj5oqmtu0ql4gc9.apps.googleusercontent.com';
 
 export const folderMimeType = 'application/vnd.google-apps.folder';
